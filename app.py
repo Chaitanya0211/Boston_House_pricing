0ae570868bf3f9c7d5abc1b8f834310d1bd7e80c
Chaitanya0211 <chaitanya.talathi02@gmail.com>
2024-04-27 17:51:07 +0530

    This is the commit for web application

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 # load the model
 reg_model = pickle.load(open("regmodel.pkl", "rb"))
 scalar = pickle.load(open("scaling.pkl", "rb"))
+feature = pickle.load(open("feature_main.pkl", "rb"))
 
 
 @app.route("/")
@@ -20,21 +21,23 @@ def predict_api():
     print(data)
     print(np.array(list(data.values())).reshape(1, -1))
     new_data = scalar.transform(np.array(list(data.values())).reshape(1, -1))
-    print(new_data)
-    output = reg_model.predict(new_data)
+    predict_data = feature.transform(np.array(list(new_data.values())).reshape(1, -1))
+    print(predict_data)
+    output = reg_model.predict(predict_data)
     print(output[0])
     return jsonify(output[0])
 
 
-# @app.route("/predict", methods=["POST"])
-# def predict():
-#     data = [float(x) for x in request.form.values()]
-#     final_input = scalar.transform(np.array(data).reshape(1, -1))
-#     print(final_input)
-#     output = reg_model.predict(final_input)[0]
-#     return render_template(
-#         "home.html", prediction_text="The House price prediction is {}".format(output)
-#     )
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scalar.transform(np.array(data).reshape(1, -1))
+    final_input_1 = feature.transform(np.array(final_input).reshape(1, -1))
+    print(final_input_1)
+    output = reg_model.predict(final_input_1)[0]
+    return render_template(
+        "home.html", prediction_text="The House price prediction is {}".format(output)
+    )
 
 
 if __name__ == "__main__":
